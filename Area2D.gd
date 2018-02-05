44138d8497ec2bca21c63a8202c24b19504a980a
Brent <brentahughson@gmail.com>
2018-02-05 08:28:58 -0500

    ws works

---
+++ Area2D.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton and event.is_action('click'):
+		get_parent().complete_action_click()
+	elif event is InputEventMouseMotion and !get_parent().stateLocal['hovered']:
+		get_parent().gameNode.setState({'hovered':get_parent().id})
