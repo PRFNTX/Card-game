ef77c8fb7e7d8988d09bd43f9ca2edaaecc146cc
Brent <brentahughson@gmail.com>
2018-02-04 20:22:53 -0500

    update endpoint decks

--- server/server.js
+++ server/server.js
@@ -60,7 +60,7 @@ app.post('/decks/:name', authenticate,(req,res)=>{
     Deck.create({
         username:user,
         deck_name:req.params.name,
-        cards:JSON.parse(deckList)
+        cards:deckList
     }).then(
         ret=>{
             res.send('deck')
